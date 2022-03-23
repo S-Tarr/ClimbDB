dd7a4b9f7ad7bf612d8016759925fe03041b5773
Rishav Bhattacharjee <brishav42@gmail.com>
2022-03-22 20:52:15 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Climber from './Climber.js';
+import Default from './Default.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Default />}/>
+        <Route path="/climber" element={<Climber />}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/Climber.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Climber() {
+	return (
+		<>
+			<p>
+				Enter climber name
+			</p>
+			<input type="text"/>
+			<button type='submit'>Submit</button>
+		</>
+	);
+}
+
+export default Climber;
