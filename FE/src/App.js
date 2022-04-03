0c962b702231633f01beeef9723b738948c0c39e
liuhoo <alexliulxl@gmail.com>
2022-04-03 12:34:54 -0400

    Added Nav Bar and CSS

--- FE/src/App.js
+++ FE/src/App.js
@@ -3,10 +3,12 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import ClimberList from "./components/ClimberList";
 import AddClimber from "./components/AddClimber";
 import EditClimber from "./components/EditClimber";
- 
+import MainPage from "./components/MainPage.js";
+import NavBar from "./components/NavBar.js";
 function App() {
   return (
     <Router>
+      <NavBar />
     <div className="container">
       <div className="columns">
         <div className="column is-half is-offset-one-quarter">
@@ -14,6 +16,7 @@ function App() {
             <Route exact path="/" element={<ClimberList />}/>
             <Route path="/add" element={<AddClimber />}/>
             <Route path="/edit/:id" element={<EditClimber />}/>
+            <Route path="/main" element={<MainPage />}/>
           </Routes>
         </div>
       </div>
---
+++ FE/src/components/MainPage.js
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from 'react'
+import axios from "axios";
+import { BrowserRouter as Router,Link } from "react-router-dom";
+import classes from './MainPage.module.css'
+const MainPage = () => {
+	
+ 
+	return (
+        <div>
+          MAINPAGE
+        </div>
+        );
+ 
+	
+}
+ 
+export default MainPage
