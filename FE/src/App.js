b979b53b97823d93fae59a6ce761394be9e56ab7
Rishav Bhattacharjee <brishav42@gmail.com>
2022-04-28 17:40:02 -0400

    Added basic context functionality

--- FE/src/App.js
+++ FE/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { useState } from 'react';
 import ClimberList from "./components/ClimberList";
 import AddClimber from "./components/AddClimber";
 import EditClimber from "./components/EditClimber";
@@ -8,26 +9,36 @@ import NavBar from "./components/NavBar.js";
 import EventList from "./components/WCCList";
 import RankList from "./components/RankList";
 import LoginPage from "./components/LoginPage";
+
+export const UserContext = React.createContext({
+  isAdmin: false,
+  toggle: () => {}
+});
+
 function App() {
+  const [isAdmin, toggle] = useState(false);
+  const ctx = {isAdmin, toggle}
   return (
-    <Router>
-      <NavBar />
-    <div className="container">
-      <div className="columns">
-        <div className="column is-half is-offset-one-quarter">
-          <Routes>
-          <Route path="/main" element={<MainPage />}/>
-            <Route exact path="/" element={<ClimberList />}/>
-            <Route path="/add" element={<AddClimber />}/>
-            <Route path="/edit/:id" element={<EditClimber />}/>
-            <Route path="/events" element={<EventList />}/>
-            <Route path="/ranks" element={<RankList />}/>
-            <Route path="/login" element={<LoginPage />}/>
-          </Routes>
+    <UserContext.Provider value={ctx}>
+      <Router>
+        <NavBar />
+      <div className="container">
+        <div className="columns">
+          <div className="column is-half is-offset-one-quarter">
+            <Routes>
+            <Route path="/main" element={<MainPage />}/>
+              <Route exact path="/" element={<ClimberList />}/>
+              <Route path="/add" element={<AddClimber />}/>
+              <Route path="/edit/:id" element={<EditClimber />}/>
+              <Route path="/events" element={<EventList />}/>
+              <Route path="/ranks" element={<RankList />}/>
+              <Route path="/login" element={<LoginPage />}/>
+            </Routes>
+          </div>
         </div>
       </div>
-    </div>
-    </Router>
+      </Router>
+    </UserContext.Provider>
   );
 }
  
--- FE/src/components/LoginPage.js
+++ FE/src/components/LoginPage.js
@@ -1,27 +1,37 @@
 import React, { useState, useEffect } from 'react';
+import { UserContext } from '../App';
+import { useContext } from 'react';
 
 const LoginPage = () => {
-	return(
-		<div>
-			<h1>Enter Login Information</h1>
-			<form>
-				<div>
-					<label>Username</label>
-					<input
-						type="text"
-						placeholder="Username"
-					/>
-				</div>
-				<div>
-					<label>Password</label>
-					<input
-						type="password"
-					/>
-				</div>
-				<button className="button is-primary">Login</button>
-			</form>
-		</div>
-	);
+	const userInfo = useContext(UserContext);
+
+	if(!userInfo.isAdmin) {
+		return(
+			<div>
+				<h1>Enter Login Information</h1>
+				<form onSubmit={() => userInfo.toggle(!userInfo.isAdmin)}>
+					<div>
+						<label>Username</label>
+						<input
+							type="text"
+							placeholder="Username"
+						/>
+					</div>
+					<div>
+						<label>Password</label>
+						<input
+							type="password"
+						/>
+					</div>
+					<button className="button is-primary">Login</button>
+				</form>
+			</div>
+		);
+	} else {
+		return(
+			<button onClick={() => userInfo.toggle(!userInfo.isAdmin)}>Toggle</button>
+		);
+	}
 }
 
 export default LoginPage;
