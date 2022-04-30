0123c643bff0be81fba2132db02f0fb15b5528ea
liuhoo <alexliulxl@gmail.com>
2022-04-30 16:02:06 -0400

    Ranks Functionality by Year

--- BE/controllers/RankController.js
+++ BE/controllers/RankController.js
@@ -5,12 +5,20 @@ const sequelize = new Sequelize('climb', 'root', 'root', {
     host: 'localhost',
     dialect:'mysql'
   });
-export const getRanks= async (req, res) => {
+export const getRanks = async (req, res) => {
     Climbers.hasOne(Ranks, {foreignKey:'ClimberID'});
     Ranks.belongsTo(Climbers);
     
 	try {
-		const ranks = await sequelize.query("SELECT Climbers.id, Climbers.name, Ranks.Points FROM `Ranks` JOIN `Climbers` ON Ranks.ClimberID = Climbers.id WHERE Ranks.SYear = 2021 ORDER BY Ranks.Points DESC LIMIT 50", { type: QueryTypes.SELECT });
+		if(req.params.SYear){
+			await sequelize.query('SET @i = ' + req.params.SYear+';');
+			console.log(req.params.SYear)
+		} else {
+			await sequelize.query('SET @i = 2021;');
+			console.log("2021")
+		}
+		
+		const ranks = await sequelize.query("SELECT Climbers.id, Climbers.name, Ranks.Points FROM `Ranks` JOIN `Climbers` ON Ranks.ClimberID = Climbers.id  WHERE Ranks.SYear = @i ORDER BY Ranks.Points DESC LIMIT 50", { type: QueryTypes.SELECT });
 
         
 		res.json(ranks);
